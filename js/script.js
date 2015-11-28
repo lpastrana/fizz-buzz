6a4fad56e320535d239586d5071a94ff98c287a3
Luis Pastrana <lpastrana2@hotmail.com>
2015-11-28 12:27:41 -0500

    Updates to index, styles and added other features to the script file.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
-
 function test() {
-  // uses the var text as the place holder for the output	
-  var text = '';
-  // for loop that sets i to 1, runs until i reaches 100 and add 1 every loop
-	  for(var i = 1; i <= 100; i++) {
-	  	// var 'tres' and 'cinco' are there to find numbers that are divisible by 3 and 5
+	// have a prompt to collect the number that will be utilized in the game
+	var inputNum = prompt("Enter a number", "Use only numbers");
+	if (isNaN(inputNum)) {
+		// if it is not a number an alert will tell the user to try again
+        alert("Please click 'Start' and enter a number.");
+        }   
+	// uses the var text as the place holder for the output	
+	var text = '';
+	// for loop that sets i to 1, runs until i reaches the number from 'inputNum' and add 1 every loop
+	for(var i = 1; i <= Math.round(inputNum); i++) {
+		// var 'tres' and 'cinco' are there to find numbers that are divisible by 3 and 5
 	    var tres = (i % 3);
-	    var cinco = (i % 5);
+	    var cinco = (i % 5);        
 	    	//if-else statement to determine if the numbers meet any of the following criteria
 		  	if (tres == 0 && cinco == 0) {
 		  		// if 'tres' and 'cinco' are divisible by the same number place that data
@@ -29,6 +34,7 @@ function test() {
 		// once the 'button - Run' is clicked, the function 'test' is executed and the 
 		// return is referenced by the Id 'game' which then gets set into the variable
 		// 'text' and displayed in the p tag
-		document.getElementById('game').innerHTML = text;
+   		// var inputNum = document.getElementById("myNum").innerHTML;  
+		document.getElementById('game').innerHTML = text;     
 		}
-}
+  }
